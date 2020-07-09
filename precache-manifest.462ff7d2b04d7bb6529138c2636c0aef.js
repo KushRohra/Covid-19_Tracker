9ecaa3a0f64b2ddc5bdd3cf075273ccf1c7c91cc
KushRohra <iec2017024@iiita.ac.in>
2020-07-09 18:18:02 +0530

    Updates

---
+++ precache-manifest.462ff7d2b04d7bb6529138c2636c0aef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bbcefbb88e7adeca0dfe139d9c04f5f",
+    "url": "/Covid-19_Tracker/index.html"
+  },
+  {
+    "revision": "7fdc9c892b098278b5a5",
+    "url": "/Covid-19_Tracker/static/css/main.f931ba5f.chunk.css"
+  },
+  {
+    "revision": "c891d01804a072287ccb",
+    "url": "/Covid-19_Tracker/static/js/2.30b5a4ab.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19_Tracker/static/js/2.30b5a4ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fdc9c892b098278b5a5",
+    "url": "/Covid-19_Tracker/static/js/main.7257265b.chunk.js"
+  },
+  {
+    "revision": "7e3e4bcef3fa6a55b0de",
+    "url": "/Covid-19_Tracker/static/js/runtime-main.0b8f3c90.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-19_Tracker/static/media/image.d7265326.png"
+  }
+]);
